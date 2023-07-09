1760ee71e70ebe221db36254e3d5c0c0a18dad30
Paul Logston <paul.logston@gmail.com>
2023-07-09 11:51:51 -0700

    tests: Finish validate_settings

--- src/epson_connect/printer_settings.py
+++ src/epson_connect/printer_settings.py
@@ -80,51 +80,20 @@ def merge_with_default_settings(settings=None):
     if not print_setting:
         return settings
 
-    # Media Size
-    print_setting['media_size'] = print_setting.get('media_size') or 'ms_a4'
 
-    # media_type
-    print_setting['media_type'] = print_setting.get('media_type') or 'mt_plainpaper'
-
-    # borderless
-    print_setting['borderless'] = print_setting.get('borderless') or False
-
-    # print_quality
-    print_setting['print_quality'] = print_setting.get('print_quality') or 'normal'
-
-    # print_quality
-    print_setting['source'] = print_setting.get('source') or 'auto'
-
-    # color_mode
-    print_setting['color_mode'] = print_setting.get('color_mode') or 'color'
-
-    # two_sided
-    print_setting['2_sided'] = print_setting.get('2_sided') or 'none'
-
-    # reverse_order
-    reverse_order = print_setting.get('reverse_order')
-    if reverse_order is None:
-        reverse_order = False
-
-    if print_setting['2_sided'] in ('long', 'short'):
-        reverse_order = False
-
-    print_setting['reverse_order'] = reverse_order
-
-    # copies
-    print_setting['copies'] = print_setting.get('copies') or 1
-
-    # collate
     collate = print_setting.get('collate')
-    if collate is None:
-        collate = True
-
-    if print_setting['2_sided'] in ('long', 'short'):
-        collate = True
-
-    print_setting['collate'] = collate
-
-    settings['print_setting'] = print_setting
+    settings['print_setting'] = {
+        'media_size': print_setting.get('media_size') or 'ms_a4',
+        'media_type': print_setting.get('media_type') or 'mt_plainpaper',
+        'borderless': print_setting.get('borderless') or False,
+        'print_quality': print_setting.get('print_quality') or 'normal',
+        'source': print_setting.get('source') or 'auto',
+        'color_mode': print_setting.get('color_mode') or 'color',
+        '2_sided': print_setting.get('2_sided') or 'none',
+        'reverse_order': print_setting.get('reverse_order') or False,
+        'copies': print_setting.get('copies') or 1,
+        'collate': collate if collate is not None else True,
+    }
 
     return settings
 
@@ -143,7 +112,7 @@ def validate_settings(settings: dict):
 
     print_mode = settings['print_mode']
     if print_mode not in VALID_PRINT_MODES:
-        raise PrintSettingError(f'Not a valid print mode {print_mode}')
+        raise PrintSettingError(f'Invalid print mode {print_mode}')
 
     # print_setting is optional so we can exit early if its not present.
     print_setting = settings.get('print_setting')
@@ -170,7 +139,7 @@ def validate_settings(settings: dict):
     if print_quality not in VALID_PRINT_QUALITIES:
         raise PrintSettingError(f'Invalid print quality {print_quality}')
 
-    # print_quality
+    # Paper source
     source = print_setting['source']
     if source not in VALID_PAPER_SOURCES:
         raise PrintSettingError(f'Invalid source {source}')
@@ -190,6 +159,9 @@ def validate_settings(settings: dict):
     if not isinstance(reverse_order, bool):
         raise PrintSettingError(f'Reverse order must be a bool')
 
+    if print_setting['2_sided'] in ('long', 'short') and reverse_order:
+        raise PrintSettingError(f'Can not use reverse order when using two-sided printing.')
+
     # copies
     copies = print_setting['copies']
     if copies < 1 or copies > 99:
@@ -200,6 +172,8 @@ def validate_settings(settings: dict):
     if not isinstance(collate, bool):
         raise PrintSettingError(f'Collate must be a bool')
 
+    if print_setting['2_sided'] in ('long', 'short') and not collate:
+        raise PrintSettingError(f'Must collate when using two-sided printing.')
 
 class PrintSettingError(ValueError):
     pass
--- tests/test_printer_settings.py
+++ tests/test_printer_settings.py
@@ -1,7 +1,14 @@
 import random
 
-from epson_connect.printer_settings import merge_with_default_settings
+import pytest
 
+from epson_connect.printer_settings import (
+    PrintSettingError,
+    merge_with_default_settings,
+    validate_settings,
+)
+
+# Fix randomized job names for tests.
 random.seed(0)
 
 
@@ -72,25 +79,6 @@ def test_merge_with_default_settings_with_full_overrides():
             'media_size': 'ms_postcard',
             '2_sided': 'long',
             'borderless': True,
-            'collate': True,  # Forced true by 2-sided printing.
-            'color_mode': 'mono',
-            'copies': 45,
-            'media_type': 'mt_hagaki',
-            'print_quality': 'draft',
-            'reverse_order': False, # Forced false by 2-sided printing.
-            'source': 'rear',
-        },
-    }
-
-
-def test_merge_with_default_settings_with_full_overrides_non_two_sided():
-    settings = merge_with_default_settings({
-        'job_name': 'job-123',
-        'print_mode': 'photo',
-        'print_setting': {
-            'media_size': 'ms_postcard',
-            '2_sided': 'none',
-            'borderless': True,
             'collate': False,
             'color_mode': 'mono',
             'copies': 45,
@@ -98,21 +86,225 @@ def test_merge_with_default_settings_with_full_overrides_non_two_sided():
             'print_quality': 'draft',
             'reverse_order': True,
             'source': 'rear',
+        },
+    }
+
+
+def test_validate_settings():
+    with pytest.raises(PrintSettingError) as e:
+        validate_settings({
+            'extra-key': None,
+        })
+    assert "Invalid settings keys {'extra-key'}." == str(e.value)
+
+    with pytest.raises(PrintSettingError) as e:
+        validate_settings({
+            'job_name': ''.join('a' for _ in range(300)),
+        })
+    assert 'Job name is greater than 256 chars' in str(e.value)
+
+    with pytest.raises(PrintSettingError) as e:
+        validate_settings({
+            'job_name': 'abc123',
+            'print_mode': 'non-mode',
+        })
+    assert 'Invalid print mode' in str(e.value)
+
+    with pytest.raises(PrintSettingError) as e:
+        validate_settings({
+            'job_name': 'abc123',
+            'print_mode': 'document',
+            'print_setting': {
+                'media_size': '1',
+            },
+        })
+    assert 'Invalid paper size' in str(e.value)
+
+    with pytest.raises(PrintSettingError) as e:
+        validate_settings({
+            'job_name': 'abc123',
+            'print_mode': 'document',
+            'print_setting': {
+                'media_size': 'ms_a4',
+                'media_type': '1',
+            },
+        })
+    assert 'Invalid media type' in str(e.value)
+
+    with pytest.raises(PrintSettingError) as e:
+        validate_settings({
+            'job_name': 'abc123',
+            'print_mode': 'document',
+            'print_setting': {
+                'media_size': 'ms_a4',
+                'media_type': 'mt_plainpaper',
+                'borderless': 'False',
+            },
+        })
+    assert 'borderless must be a bool' in str(e.value)
+
+    with pytest.raises(PrintSettingError) as e:
+        validate_settings({
+            'job_name': 'abc123',
+            'print_mode': 'document',
+            'print_setting': {
+                'media_size': 'ms_a4',
+                'media_type': 'mt_plainpaper',
+                'borderless': False,
+                'print_quality': 'bad',
+            },
+        })
+    assert 'Invalid print quality' in str(e.value)
+
+    with pytest.raises(PrintSettingError) as e:
+        validate_settings({
+            'job_name': 'abc123',
+            'print_mode': 'document',
+            'print_setting': {
+                'media_size': 'ms_a4',
+                'media_type': 'mt_plainpaper',
+                'borderless': False,
+                'print_quality': 'high',
+                'source': 'up',
+            },
+        })
+    assert 'Invalid source' in str(e.value)
+
+    with pytest.raises(PrintSettingError) as e:
+        validate_settings({
+            'job_name': 'abc123',
+            'print_mode': 'document',
+            'print_setting': {
+                'media_size': 'ms_a4',
+                'media_type': 'mt_plainpaper',
+                'borderless': False,
+                'print_quality': 'high',
+                'source': 'rear',
+                'color_mode': 'vibrant',
+            },
+        })
+    assert 'Invalid color mode' in str(e.value)
+
+    with pytest.raises(PrintSettingError) as e:
+        validate_settings({
+            'job_name': 'abc123',
+            'print_mode': 'document',
+            'print_setting': {
+                'media_size': 'ms_a4',
+                'media_type': 'mt_plainpaper',
+                'borderless': False,
+                'print_quality': 'high',
+                'source': 'rear',
+                'color_mode': 'mono',
+                '2_sided': 'both',
+            },
+        })
+    assert 'Invalid 2-sided value' in str(e.value)
+
+    with pytest.raises(PrintSettingError) as e:
+        validate_settings({
+            'job_name': 'abc123',
+            'print_mode': 'document',
+            'print_setting': {
+                'media_size': 'ms_a4',
+                'media_type': 'mt_plainpaper',
+                'borderless': False,
+                'print_quality': 'high',
+                'source': 'rear',
+                'color_mode': 'mono',
+                '2_sided': 'long',
+                'reverse_order': 'yes',
+            },
+        })
+    assert 'Reverse order must be a bool' in str(e.value)
+
+    with pytest.raises(PrintSettingError) as e:
+        validate_settings({
+            'job_name': 'abc123',
+            'print_mode': 'document',
+            'print_setting': {
+                'media_size': 'ms_a4',
+                'media_type': 'mt_plainpaper',
+                'borderless': False,
+                'print_quality': 'high',
+                'source': 'rear',
+                'color_mode': 'mono',
+                '2_sided': 'long',
+                'reverse_order': True,
+            },
+        })
+    assert 'Can not use reverse order when using two-sided printing.' in str(e.value)
+
+    with pytest.raises(PrintSettingError) as e:
+        validate_settings({
+            'job_name': 'abc123',
+            'print_mode': 'document',
+            'print_setting': {
+                'media_size': 'ms_a4',
+                'media_type': 'mt_plainpaper',
+                'borderless': False,
+                'print_quality': 'high',
+                'source': 'rear',
+                'color_mode': 'mono',
+                '2_sided': 'long',
+                'reverse_order': False,
+                'copies': 100,
+            },
+        })
+    assert 'Invalid number of copies' in str(e.value)
+
+    with pytest.raises(PrintSettingError) as e:
+        validate_settings({
+            'job_name': 'abc123',
+            'print_mode': 'document',
+            'print_setting': {
+                'media_size': 'ms_a4',
+                'media_type': 'mt_plainpaper',
+                'borderless': False,
+                'print_quality': 'high',
+                'source': 'rear',
+                'color_mode': 'mono',
+                '2_sided': 'long',
+                'reverse_order': False,
+                'copies': 45,
+                'collate': 'yes',
+            },
+        })
+    assert 'Collate must be a bool' in str(e.value)
+
+    with pytest.raises(PrintSettingError) as e:
+        validate_settings({
+            'job_name': 'abc123',
+            'print_mode': 'document',
+            'print_setting': {
+                'media_size': 'ms_a4',
+                'media_type': 'mt_plainpaper',
+                'borderless': False,
+                'print_quality': 'high',
+                'source': 'rear',
+                'color_mode': 'mono',
+                '2_sided': 'long',
+                'reverse_order': False,
+                'copies': 45,
+                'collate': False,
+            },
+        })
+    assert 'Must collate when using two-sided printing.' in str(e.value)
+
+    # No errors.
+    validate_settings({
+        'job_name': 'abc123',
+        'print_mode': 'document',
+        'print_setting': {
+            'media_size': 'ms_a4',
+            'media_type': 'mt_plainpaper',
+            'borderless': False,
+            'print_quality': 'high',
+            'source': 'rear',
+            'color_mode': 'mono',
+            '2_sided': 'long',
+            'reverse_order': False,
+            'copies': 45,
+            'collate': True,
         },
     })
-    assert settings == {
-        'job_name': 'job-123',
-        'print_mode': 'photo',
-        'print_setting': {
-            'media_size': 'ms_postcard',
-            '2_sided': 'none',
-            'borderless': True,
-            'collate': False,
-            'color_mode': 'mono',
-            'copies': 45,
-            'media_type': 'mt_hagaki',
-            'print_quality': 'draft',
-            'reverse_order': True,
-            'source': 'rear',
-        },
-    }
