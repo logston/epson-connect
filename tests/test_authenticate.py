b24983f08d63f35ce71be09f36321477260078ec
Paul Logston <paul.logston@gmail.com>
2023-07-12 17:41:16 -0700

    Complete test coverage for authentication

--- tests/test_authenticate.py
+++ tests/test_authenticate.py
@@ -2,6 +2,7 @@ from datetime import datetime, timedelta
 from unittest import mock
 
 import pytest
+import requests
 
 from epson_connect.authenticate import ApiError, AuthCtx, AuthenticationError
 
@@ -120,3 +121,84 @@ def test_auth_ctx_api_error():
                 client_id='ghi',
                 client_secret='789',
             )
+
+
+@mock.patch('epson_connect.authenticate.AuthCtx._auth')
+@mock.patch('requests.request')
+def test_auth_ctx_send_status_ok(req, _auth):
+    mock_resp = requests.Response()
+    mock_resp.encoding = 'utf8'
+    mock_resp._content = b'{"status": "ok"}'
+    req.return_value = mock_resp
+
+    auth_ctx = AuthCtx(
+        base_url='https://example.com/my/path',
+        printer_email='example3@print.epsonconnect.com',
+        client_id='ghi',
+        client_secret='789',
+    )
+
+    auth_ctx.send('test_method', 'test_path')
+
+    _auth.assert_called()
+
+
+@mock.patch('epson_connect.authenticate.AuthCtx._auth')
+@mock.patch('requests.request')
+def test_auth_ctx_send_api_error(req, _auth):
+    mock_resp = requests.Response()
+    mock_resp.encoding = 'utf8'
+    mock_resp._content = b'{"code": "test api problems"}'
+    req.return_value = mock_resp
+
+    auth_ctx = AuthCtx(
+        base_url='https://example.com/my/path',
+        printer_email='example3@print.epsonconnect.com',
+        client_id='ghi',
+        client_secret='789',
+    )
+
+    with pytest.raises(ApiError) as e:
+        auth_ctx.send('test_method', 'test_path')
+
+    assert 'test api problems' in str(e)
+    _auth.assert_called()
+
+
+@mock.patch('epson_connect.authenticate.AuthCtx._auth')
+@mock.patch('requests.request')
+def test_auth_ctx_send_non_json_response(req, _auth):
+    mock_resp = requests.Response()
+    mock_resp.encoding = 'utf8'
+    mock_resp._content = b'something not json'
+    req.return_value = mock_resp
+
+    auth_ctx = AuthCtx(
+        base_url='https://example.com/my/path',
+        printer_email='example3@print.epsonconnect.com',
+        client_id='ghi',
+        client_secret='789',
+    )
+
+    with pytest.raises(ApiError) as e:
+        auth_ctx.send('test_method', 'test_path')
+
+    assert 'something not json' in str(e)
+    _auth.assert_called()
+
+
+@mock.patch('epson_connect.authenticate.AuthCtx._auth')
+def test_auth_ctx_default_headers(_auth):
+    auth_ctx = AuthCtx(
+        base_url='https://example.com/my/path',
+        printer_email='example3@print.epsonconnect.com',
+        client_id='ghi',
+        client_secret='789',
+    )
+
+    auth_ctx._access_token = 'abc123'
+
+    assert auth_ctx.default_headers == {
+        'Authorization': 'Bearer abc123',
+        'Content-Type': 'application/json',
+    }
